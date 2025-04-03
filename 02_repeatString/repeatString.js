2c45b25612042c545453451f907cc47d9c4d3e41
josniff <186874932+josniff@users.noreply.github.com>
2025-04-03 15:15:23 -0500

    complete the test "repeats the string"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let longString = "";
+    let i = 0;
+    while (i < num) {
+        longString += "hey";
+        ++i;
+    };
+    return longString;
+};  
 
-};
+
+
+
+
+
+
+// 3 times, add the 'hey' to the end of the string
+// then, take the long string (heyheyhey) and return it to the caller
 
 // Do not edit below this line
 module.exports = repeatString;
