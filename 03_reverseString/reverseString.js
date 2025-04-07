d668c769653b369302a2c12c8b108f46f061dd7b
josniff <186874932+josniff@users.noreply.github.com>
2025-04-07 17:01:35 -0500

    complete all the tests for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 };
+let string = '';
 
 // Do not edit below this line
 module.exports = reverseString;
