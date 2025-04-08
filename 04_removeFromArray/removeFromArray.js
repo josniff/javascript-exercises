09519957d1dd26779a9b281048c29d56df49204e
josniff <186874932+josniff@users.noreply.github.com>
2025-04-08 14:16:21 -0500

    complete the test 'removes a single value'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+// removes a single value
+const removeFromArray = function(num, index) {
+    num.splice (index - 1, 1);
+    return num; 
 };
 
 // Do not edit below this line
