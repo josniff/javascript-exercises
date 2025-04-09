eff3dec77a03b3d0f4726b71c3f5f9a022c2f800
josniff <186874932+josniff@users.noreply.github.com>
2025-04-08 19:18:18 -0500

    complete the exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = 0;
+    if (num1 < 0 || !Number.isInteger(num1) || typeof num2 != "number") {
+        return 'ERROR';
+    }
+   else if (num1 < num2) {
+    let i = num1;
+    while (i <= num2) {
+        sum += i;
+        i++;
+    };
+} else {
+    let i = num1;
+    while (i >= num2) {
+        sum += i;
+        i--;
+    };
+} 
+    return sum;
+    
 };
 
 // Do not edit below this line
