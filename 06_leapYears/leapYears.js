60d3151ae37c8aa6f0a150d76003ae3b803d986c
josniff <186874932+josniff@users.noreply.github.com>
2025-04-08 19:36:39 -0500

    complete the tests for leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    result = year % 4;
+    if (result === 0 && result != year % 10 || result === year % 400){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
